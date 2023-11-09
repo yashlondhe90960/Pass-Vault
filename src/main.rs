ccc82eb49ab940593b730ba49a51268046fdf465
Yash Londhe <yashlondhe9151@gmail.com>
2023-11-10 02:59:09 +0530

    added the working code(not connected to db)

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,89 @@ mod pentry;
 
 use crate::pentry::prompt;
 use crate::pentry::read_passwords_from_file;
+use crate::pentry::ServiceInfo;
 
-fn clr(){
+fn clr() {
     print!("{}[23", 27 as char);
-
 }
 fn main() {
     clr();
     let ascii = r#"
-    
- ________  ________  ________   ________           ___      ___ ________  ___  ___  ___   _________       
- |\   __  \|\   __  \|\   ____\ |\   ____\         |\  \    /  /|\   __  \|\  \|\  \|\  \ |\___   ___\     
- \ \  \|\  \ \  \|\  \ \  \___|_\ \  \___|_        \ \  \  /  / | \  \|\  \ \  \\\  \ \  \\|___ \  \_|     
-  \ \   ____\ \   __  \ \_____  \\ \_____  \        \ \  \/  / / \ \   __  \ \  \\\  \ \  \    \ \  \      
-   \ \  \___|\ \  \ \  \|____|\  \\|____|\  \        \ \    / /   \ \  \ \  \ \  \\\  \ \  \____\ \  \     
-    \ \__\    \ \__\ \__\____\_\  \ ____\_\  \        \ \__/ /     \ \__\ \__\ \_______\ \_______\ \__\    
-     \|__|     \|__|\|__|\_________\\_________\        \|__|/       \|__|\|__|\|_______|\|_______|\|__|    
-                        \|_________\|_________|                                                            
-                                                                                                           
-                                                                                                           
+
+     ____                __     __          _ _    
+    |  _ \ __ _ ___ ___  \ \   / /_ _ _   _| | |_
+    | |_) / _` / __/ __|  \ \ / / _` | | | | | __|
+    |  __/ (_| \__ \__ \   \ V / (_| | |_| | | |_ 
+    |_|   \__,_|___/___/    \_/ \__,_|\__,_|_|\__|
+                                 
  
     "#;
+    println!("{ascii}");
+    loop {
+        println!("Password manager menu: ");
+        println!("1. Add Entry");
+        println!("2. List Entries");
+        println!("3. Search Entry ");
+        println!("4. Quit!!! ");
 
+        let mut choice = String::new();
+        std::io::stdin().read_line(&mut choice).unwrap();
+        match choice.trim() {
+            "1" => {
+                clr();
+                let entry = ServiceInfo::new(
+                    prompt("Service :"),
+                    prompt("Username :"),
+                    prompt("Password :"),
+                );
+                println!("Entry added successfully");
+                entry.write_to_file();
+            }
+            "2" => {
+                clr();
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords: {}", err);
+                    Vec::new()
+                });
+                // let services = read_passwords_from_file().unwrap_or_else(|err| {
+                //     eprintln!("Error reading passwords: {}", err);
+                //     Vec::new()
+                // });
+
+                for item in &services {
+                    println!(
+                        "Service = {}
+                            - Username : {}
+                            - Password : {} ",
+                        item.service, item.username, item.password
+                    );
+                }
+            }
+            "3" => {
+                clr();
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords:{}", err);
+                    Vec::new()
+                });
+                let search = prompt("Search :");
+                for item in &services {
+                    if item.service.as_str() == search.as_str() {
+                        println!(
+                            "Service = {}
+                        - Username : {}
+                        - Password : {}",
+                            item.service, item.username, item.password
+                        );
+                    }
+                }
+            }
+            "4" => {
+                clr();
+                println!("Good bye!");
+                break;
+            }
+            _ => println!("Invalid Choice."),
+        }
+        println!("\n\n");
+    }
 }
-
-
